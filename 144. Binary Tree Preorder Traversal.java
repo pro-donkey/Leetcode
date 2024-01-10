986c5340e4bd0616606b7f06a6696537fe65cfaf
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-01-10 22:47:01 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- 144. Binary Tree Preorder Traversal.java
+++ 144. Binary Tree Preorder Traversal.java
@@ -39,3 +39,46 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+// below is the recurssive code 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public void dfs(List<Integer> ans , TreeNode root){
+
+       //  List<Integer> preOrder = new ArrayList<>(); 
+        if(root == null ) return ;
+
+        ans.add(root.val);
+        dfs(ans, root.left);
+        dfs(ans, root.right);
+
+    }
+
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preOrder = new ArrayList<>();
+
+        dfs(preOrder, root);
+
+        return preOrder;
+    }
+}
