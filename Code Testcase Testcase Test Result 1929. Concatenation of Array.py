3f3e6a7881e3b3a1f3e169bcfcd468e6c2be72c0
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-02-04 19:44:44 +0530

    1929. Concatenation of Array.py

---
+++ Code Testcase Testcase Test Result 1929. Concatenation of Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(0,len(nums)):
+            ans.append(nums[i])
+        
+        for i in range(0,len(nums)):
+            ans.append(nums[i])
+        
+        return ans
+        
