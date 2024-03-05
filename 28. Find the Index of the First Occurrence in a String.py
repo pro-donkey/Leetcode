d4f717d2c5974fe6f7a1ab4e380fab516fb52fdf
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-03-05 21:56:31 +0530

    Create 28. Find the Index of the First Occurrence in a String.py

---
+++ 28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        if len(needle) > len(haystack):
+            return -1
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
