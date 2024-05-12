550282f0c619e6dc38df6526774f9f2acfa57763
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2024-05-12 22:42:14 +0530

    Create 77. Combinations.py

---
+++ 77. Combinations.py
@@ -0,0 +1,6 @@
+from itertools import combinations
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(combinations([x for x in range(1, n + 1)], k))
