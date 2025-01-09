80fd189578b6631f4cb468075ec2813416e0e022
Pro Donkey <153301727+pro-donkey@users.noreply.github.com>
2025-01-09 13:24:47 +0530

    Create 1408. String Matching in an Array.py

---
+++ 1408. String Matching in an Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+
+        ans = []
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j:
+                    continue 
+                else:
+                    if words[i] in words[j]:
+                        ans.append(words[i])
+                        break 
+            
+        
+        return ans
+        
+        
+        
